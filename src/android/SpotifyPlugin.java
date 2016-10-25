0a1226db3ec281caeefea86f63f68c0fc3f9cd36
aleksey stepanets <aleksey stepanets>
2016-10-25 11:07:33 +0300

    added SpotifyPluginController

--- src/android/SpotifyPlugin.java
+++ src/android/SpotifyPlugin.java
@@ -1,8 +1,12 @@
 package cordova.plugin.spotify;
 
+import android.util.Log;
+
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,22 +15,22 @@ import org.json.JSONObject;
  * This class echoes a string called from JavaScript.
  */
 public class SpotifyPlugin extends CordovaPlugin {
-
+    //final  private string TAG = "SpotifyPlugin";
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView){
+        super.initialize(cordova,webView);
+        Log.i("SpotifyPlugin:"," initialize");
+        //// TODO: 25.10.2016
+    }
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+            
+            
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
-    }
+    
 }
